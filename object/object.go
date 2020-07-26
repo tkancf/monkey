255391666ea1a29fe82206c4288f94f6346e95e6
tkancf <tkncf789@gmail.com>
2020-07-26 21:57:18 +0900

    3.7 return satement

--- object/object.go
+++ object/object.go
@@ -5,10 +5,18 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURY_VALUE_OBJ = "RETURN_VALUE"
 )
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURY_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
 
 type Object interface {
 	Type() ObjectType
